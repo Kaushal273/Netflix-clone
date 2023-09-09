564e5baa9c53e2706b63e60d887cb6c371e9247e
Kaushal273 <kaushalyadav536@gmail.com>
2023-09-10 01:05:40 +0530

    header

--- src/Nav.js
+++ src/Nav.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Nav.css';
+import { useState } from 'react';
 
 function Nav() {
+
+    const[show, handleShow] = useState(false);
+
+    const transitionNavBar = () => {
+        if (window.scrollY > 100) {
+            handleShow(true);
+        } else {
+            handleShow(false)
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener("scroll", transitionNavBar);
+        return () => window.removeEventListener("scroll", transitionNavBar);
+    },[])
+
     return (
-        <div className='nav nav__black'>
+        <div className={`nav ${show && "nav__black"}`}>
             <div className="nav__contents">
-                <img src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+                <img src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png"
                     className='nav__logo'
                     alt="logo"
                 />
